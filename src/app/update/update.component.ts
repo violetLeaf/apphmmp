07790b153a3110c6d0cd03c275a4521b6913dc0e
Caethe <c.raven@agir.at>
2020-02-25 00:03:56 +0100

    24 02 20 / 1
    Anpassen der App auf die API, damit Ausgaben passen
    -- es muss noch die update seite angepasst werden, da dort noch fehler auftreten

--- src/app/update/update.component.ts
+++ src/app/update/update.component.ts
@@ -2,6 +2,9 @@ declare var downloader: any;
 declare var cordova: any;
 
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import TourModel from '../shared/tour.model';
 
 @Component({
   selector: 'app-update',
@@ -9,15 +12,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update.component.scss']
 })
 export class UpdateComponent implements OnInit {
+  tours: TourModel[] = [];
 
-  constructor() { }
+  constructor(private router: Router, private http: HttpClient) {
+    // clear all downloaded Tours
+    this.http.delete("http://localhost:3000/deletetoursapp").subscribe(function(res) {
+     console.log(res);
+    }.bind(this));
+
+    // get all Tours
+    this.http.get<TourModel[]>("http://localhost:3000/toursapp").subscribe(function(res) {
+     this.tours = res;
+     console.log(res);
+     console.log(this.tours);
+    }.bind(this));
+  }
   
   todisplay:string;
+  path:string = "";
 
   ngOnInit() {
     // code für cordova, plugin in cordova
     downloader.init({folder: "media", fileSystem: cordova.file.dataDirectory, unzip: true});
-    downloader.get("https://file-examples.com/wp-content/uploads/2017/02/zip_2MB.zip");
+    // downloader.get("https://file-examples.com/wp-content/uploads/2017/02/zip_2MB.zip");
+    this.tours.forEach(tour => {
+      downloader.get("http://localhost:3000/download/" + tour.id);
+      this.path += "media/tour_" + tour.id + ";";
+    });
+
+    // removes the last ;
+    this.path = this.path.slice(0, -1);
+    console.log(this.path);
 
     document.addEventListener("DOWNLOADER_error", function(event){
       console.log("downloader_error: ");
@@ -34,12 +59,9 @@ export class UpdateComponent implements OnInit {
       console.log(event);
       this.todisplay = "https://http.cat/200";
 
-
-      // TODO: muss noch geändert werden, dass die eigentlichen Namen eingelesen werden
-      // var path = "media/exampleTour1;media/exampleTour2";
-      var path = "media/exampleTour1;media/exampleTour2;media/exampleTour3;media/exampleTour4";
+      // var path = "media/exampleTour1;media/exampleTour2;media/exampleTour3;media/exampleTour4";
       // localStorage wird gesetzt wo wir die einzelnen Dateien gesetzt haben
-      window.localStorage.setItem("path", path);
+      window.localStorage.setItem("path", this.path);
       
     });
   }
